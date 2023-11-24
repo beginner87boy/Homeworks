413c07cad9083d8ac340b88c1993987d0f424505
beginner87boy <omer.hesenov1987@gmail.com>
2023-11-24 18:24:59 +0400

    task 2.3 completed

--- Task2.3/task2.3.js
+++ Task2.3/task2.3.js
@@ -1,31 +1,39 @@
 //Task 1: Change the Background Color
 //// Create an HTML file with a button. When the button is clicked, change the background color of the page to a random color.
 
-let button = document.getElementsByTagName('button')[0];
+// let result = document.getElementById("GFG");
+ 
+// function changeColor(color) {
+//     document.body.style.background = color;
+// }
 
-function RBC (e) {
-  button.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-}
-
-button.addEventListener("click", RBC);
-console.log = RBC();
+// function cliced() {
+//     changeColor('yellow');
+//     result.innerHTML = "Background Color changed";
+// }    
 
 
 // Task 1 end
 
 
 
-// Task 2 
+// Task 2 Create an HTML file with a button and a counter display. When the button is clicked, increment the counter and update the display.
 
+// function changeValue(diff) {
+//   var count = document.getElementById('count');
+//   count.innerText = +count.innerText + diff;
+// }
 
 // Task 2 end
 
 
 
 
-// Task 3 
+// Task 3 Create an HTML element for the image tag and show the screen an image then create a button in HTML and write a function in JS for button click. When click the button change the image source(src) attribute
 
-
+function myFunction() {
+  document.getElementById("myImg").src = "1.child";
+}
 // Task 3 end
 
 
